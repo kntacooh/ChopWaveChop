96252be05265aecc769af7a80f9f2a528da0c431
kntacooh <kntaco03g1@gmail.com>
2014-04-28 22:37:48 +0900

    ボタンのスタイルの修正。
    ボタンのスタイルを統一して、Contentで形が変わるようにしました。
    あとSlider改造中。

---
+++ ChopWaveChop/ContextInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChopWaveChop
+{
+    public class ContextInfo
+    {
+        public bool IsPlay = false;
+
+        public ContextInfo()
+        {
+
+        }
+    }
+}
